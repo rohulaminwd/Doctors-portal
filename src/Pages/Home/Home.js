2c91bc159473379ebdd62c7d61b6b62dc55b9a44
rohulaminwd <rohulaminwd@gmail.com>
2022-05-21 23:02:14 +0600

    fist commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,7 +5,7 @@ import Footer from '../Shared/Footer';
 const Home = () => {
     return (
         <div className=''>
-            <h1>hello</h1>
+            <h1>hello word</h1>
             <Footer />
         </div>
     );
